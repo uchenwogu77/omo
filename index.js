e6d8d5b9a416b263ef360f72fe3bfe71579c16dc
uche <nwoguchibuzor01@gmail.com>
2023-07-27 20:00:55 +0100

    omo

---
+++ index.js
@@ -0,0 +1,18 @@
+var num1 = 2;
+var num2 = 4;
+
+console.log("Simple calculator");
+
+var addition = num1+num2;
+var multiplication = num1*num2;
+var  division = num1/num2;
+var subtraction = num1-num2;
+var remainder = num1%num2;
+
+console.log(addition);
+console.log(subtraction);
+console.log(division);
+console.log(multiplication);
+console.log(remainder);
+
+prompt("Enter anything");
